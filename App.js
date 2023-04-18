74f9ec50f93ba7fb0c74d74741d96268a2f3702e
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-04-18 10:49:05 -0700

    Update App.js

--- App.js
+++ App.js
@@ -64,3 +64,91 @@ const App = () => {
         }
       };
 
+      const handleDecreaseQuantity = (id) => {
+        const updatedCartItems = cartItems
+          .map((item) => (item.id === id ? { ...item, quantity: item.quantity - 1 } : item))
+          .filter((item) => item.quantity > 0);
+    
+        setCartItems(updatedCartItems);
+        database.ref(`users/${user.uid}/cartItems`).set(updatedCartItems);
+      };
+    
+      const handleSignIn = (signedInUser) => {
+        setUser(signedInUser);
+      };
+    
+      const handleSignOut = () => {
+        setUser(null);
+      };
+      const toggleCartVisibility = () => {
+        setCartVisible(!cartVisible);
+      };
+      const handleToggleCart = () => {
+        setCartVisible(!cartVisible);
+      };
+      const handleAddToCart = (book) => {
+        console.log("Handling add to cart:", book);
+        if (user) {
+          addToCart(book);
+          setCartVisible(true);
+        } else {
+          showSignInModal(); // Show the sign-in modal if the user is not logged in
+        }
+      };
+      
+      const handleIncreaseQuantity = (id) => {
+        const updatedCartItems = cartItems.map((item) =>
+          item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+        setCartItems(updatedCartItems);
+        database.ref(`users/${user.uid}/cartItems`).set(updatedCartItems);
+      };
+      const handleCheckout = () => {
+        setCheckoutModalVisible(true);
+      };
+      
+      const showSignInModal = () => {
+        setSignInModalVisible(true);
+      };
+      
+    
+      return (
+        <Router>
+          <div className="App">
+            <NavBar
+              user={user}
+              onSignOut={handleSignOut}
+              onSearchQueryChange={setSearchQuery}
+              onToggleCart={toggleCartVisibility}
+            />
+            <Home
+              searchQuery={searchQuery}
+              addToCart={handleAddToCart}
+              cartItems={cartItems}
+              user={user}
+              showSignInModal={showSignInModal} // Pass the showSignInModal function as a prop
+            />
+            <Cart
+              onIncreaseQuantity={handleIncreaseQuantity}
+              onDecreaseQuantity={handleDecreaseQuantity}
+              visible={cartVisible}
+              onClose={handleToggleCart}
+              cartItems={cartItems}
+              onCheckout={handleCheckout}
+            />
+            <CheckoutModal
+              isOpen={checkoutModalVisible}
+              onRequestClose={() => setCheckoutModalVisible(false)}
+            />
+            <SignInModal
+              isOpen={signInModalVisible}
+              onRequestClose={() => setSignInModalVisible(false)}
+            />
+          </div>
+        </Router>
+      );
+    };
+    
+    export default App;
+    
+
