cddce70ab98dadaefd807d24dc803da7cb28b9e1
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-04-18 10:43:21 -0700

    defining some state variables in App.js

--- App.js
+++ App.js
@@ -1,1 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import NavBar from './Navbar';
+import Home from './Home';
+import Cart from './Cart';
+import { auth, database } from './firebase';
+import SignInModal from './SignInModal';
+import CheckoutModal from './CheckoutModal';
 
+const App = () => {
+    const [user, setUser] = useState(null);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [cartVisible, setCartVisible] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
+    const [signInModalVisible, setSignInModalVisible] = useState(false); 
+    const [checkoutModalVisible, setCheckoutModalVisible] = useState(false);
+
+
+    
+  
+
+}
