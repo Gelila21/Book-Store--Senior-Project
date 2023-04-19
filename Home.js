c38248dec926bf044519d3895a3d14adfdca79a7
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-04-19 00:02:30 -0700

    updated home.js

---
+++ Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css';
+
+const booksData = [
+  {
+    id: 1,
+    title: 'Book1',
+    author: 'Author 1',
+    price: '$9.99',
+    cover: '/assets/book1.jpg'
+  },
+  {
+    id: 2,
+    title: 'Book2',
+    author: 'Author 2',
+    price: '$12.99',
+    cover: '/assets/book9.jpg'
+  },
