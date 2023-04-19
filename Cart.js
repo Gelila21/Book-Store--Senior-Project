3b3c4c7771aaee89c4293bc7d9e4b3ef33c69dbe
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-04-18 23:14:16 -0700

    Update Cart.js

--- Cart.js
+++ Cart.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import './Cart.css';
 
+const Cart = ({ cartItems, visible, onClose, onIncreaseQuantity, onDecreaseQuantity, onCheckout }) => {
+  const handleCloseClick = () => {
+    if (onClose) {
+      onClose();
+    }
+  };
