64b618594632c610d923a12ecec3d1afafbd84cd
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-04-19 10:58:03 -0700

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -39,3 +39,16 @@ const Navbar = ({
           unsubscribe();
         };
       }, []);
+
+      const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
