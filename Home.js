824f4645c163ab3343ef73df2761739413c1dbd3
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-04-19 00:03:05 -0700

    Update Home.js

--- Home.js
+++ Home.js
@@ -16,3 +16,46 @@ const booksData = [
     price: '$12.99',
     cover: '/assets/book9.jpg'
   },
+  {
+    id: 3,
+    title: 'Book3',
+    author: 'Author 1',
+    price: '$9.99',
+    cover: '/assets/book3.jpg'
+  },
+  {
+    id: 4,
+    title: 'Book4',
+    author: 'Author 2',
+    price: '$12.99',
+    cover: '/assets/book4.jpg'
+  },
+  {
+    id: 5,
+    title: 'Book5',
+    author: 'Author 1',
+    price: '$9.99',
+    cover: '/assets/book5.jpg'
+  },
+  {
+    id: 6,
+    title: 'Book6',
+    author: 'Author 2',
+    price: '$12.99',
+    cover: '/assets/book6.jpg'
+  },
+  {
+    id: 7,
+    title: 'Book7',
+    author: 'Author 1',
+    price: '$9.99',
+    cover: '/assets/book7.jpg'
+  },
+  {
+    id: 8,
+    title: 'Book8',
+    author: 'Author 2',
+    price: '$12.99',
+    cover: '/assets/book8.jpg'
+  }
+];
