e7acc5daee6323e10e240df829ffd8ff271706a2
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-04-19 10:55:38 -0700

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -1,1 +1,8 @@
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FaUserCircle } from "react-icons/fa";
+import "./Navbar.css";
+import SignUpModal from "./SignUpModal";
+import SignInModal from "./SignInModal";
+import { auth } from "./firebase";
+import { AiOutlineShoppingCart } from 'react-icons/ai';
