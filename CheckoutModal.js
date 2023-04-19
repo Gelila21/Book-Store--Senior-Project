5fa412e3128b89f4e0b489c01870f81a590cca27
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-04-18 23:47:06 -0700

    Update CheckoutModal.js

--- CheckoutModal.js
+++ CheckoutModal.js
@@ -1,1 +1,20 @@
+import React from "react";
+import Modal from "react-modal";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  TextField,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
+} from "@material-ui/core";
+import "./CheckoutModal.css";
 
+const useStyles = makeStyles((theme) => ({
+    modal: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    },
