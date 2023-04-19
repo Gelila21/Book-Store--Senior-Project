e1d243bd27abfc993da4e66bf0127a280c72ee51
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-04-19 11:04:22 -0700

    Update SignUpModal.js

--- SignUpModal.js
+++ SignUpModal.js
@@ -1,1 +1,20 @@
+import React, { useState } from 'react';
+import { auth, createUserProfileDocument } from './firebase';
+import {
+  Button,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from '@mui/material';
 
+const SignUpModal = ({ isOpen, onRequestClose, onUserSignUp }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
