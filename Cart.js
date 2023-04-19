265ccc4d8b6b656095ea633d606d3a61ff167fd1
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2023-04-18 23:16:26 -0700

    Update Cart.js

--- Cart.js
+++ Cart.js
@@ -7,3 +7,21 @@ const Cart = ({ cartItems, visible, onClose, onIncreaseQuantity, onDecreaseQuant
       onClose();
     }
   };
+  const handleCheckout = () => {
+    if (onCheckout) {
+      onCheckout();
+    }
+  };
+  const CartItems = ({ items, onIncreaseQuantity, onDecreaseQuantity }) => {
+    return (
+      items.length > 0 ? (
+        <div>
+          {items.map((item) => (
+            <div key={item.id} className="cart-item">
+              <img src={item.cover} alt={item.title} />
+              <div className="cart-item-info"></div>
+              <h4>{item.title}</h4>
+                <p>{item.author}</p>
+                <p>{item.description}</p> {/* Add this line to display the description */}
+                <p>{item.price} x {item.quantity} = ${(parseFloat(item.price.slice(1)) * item.quantity).toFixed(2)}</p>
+              </div>
